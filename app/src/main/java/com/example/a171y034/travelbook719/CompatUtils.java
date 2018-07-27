bc413b9eca9e301a675d6de6121c49ef514ea944
Daisuke TAKAHASHI <171y034@epson-isc.com>
2018-07-27 12:15:56 +0900

    SmoothcheckBox

---
+++ app/src/main/java/com/example/a171y034/travelbook719/CompatUtils.java
@@ -0,0 +1,15 @@
+package com.example.a171y034.travelbook719;
+
+import android.content.Context;
+
+/**
+ * Created by 171y034 on 2018/07/27.
+ */
+
+public class CompatUtils {
+    public static int dp2px(Context context, float dipValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
+
+}
